93e990abc17281f41dad65e89d45c136a37c6348
IliaVetchinin <ivetchinin@gmail.com>
2020-09-28 23:23:19 +0300

    don\t work driver builder

--- bulder.py
+++ bulder.py
@@ -2,28 +2,38 @@ from Driver import JsonFileDriver
 from Driver import CsvFileDriver
 
 
-class DriverBuilder(self):
+class DriverBuilder:
     def build(self):
         return None
 
 
-class JsonFileBuilder(self):
-    pass
+class JsonFileBuilder(DriverBuilder):
+    def build(self):
+        filename = input('Введите названия файла')
+        return JsonFileDriver(filename)
 
 
-class CsvFileBuilder(self):
-    pass
+class CsvFileBuilder(DriverBuilder):
+    def build(self):
+        filename = 'csv.csv'
+        return CsvFileDriver(filename)
 
 
-class FabricBuilders(self):
+class FabricBuilders:
     @staticmethod
     def fabric_driver():
         driver_name = input('Where you prefer save the file?')
 
-        driver = {
+        drivers = {
             'json_file': JsonFileDriver,
             'csv_file': CsvFileDriver
 
         }
 
+        return drivers[driver_name]().build()
 
+
+if __name__ == '__main__':
+
+    # driver = FabricBuilders.fabric_driver()
+    CsvFileDriver().build()
